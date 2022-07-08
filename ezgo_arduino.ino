389d3dcc0411c25aef94724074581f9b12dfb98a
Kenny <kenny012344@gmail.com>
2022-07-09 00:19:37 +0800

    Add light control function

--- ezgo_arduino.ino
+++ ezgo_arduino.ino
@@ -1,6 +1,6 @@
 /* 
  *  Testing Code for E-Z-GO Glof Car 
- *  This Package is able to control throttle and brake.
+ *  This Package is able to control throttle, brake and light.
  *  Besides, it has two control mode, manual mode, autonumous mode.
  */ 
 #include "DAC8562.h"
@@ -15,6 +15,7 @@
  * diameter   : 18 inch tire 
  * gear_ratio : gear ratio is 16.99
  * REF_POWER  : This power is for referencing by DAC8562
+ * lightPin   : This pin controls front and tail lights.
  */
 #define modePin 7 
 #define SSPin 10    
@@ -24,17 +25,19 @@
 #define pi 3.14159
 #define gear_ratio 16.99
 #define REF_POWER 3.34
+#define lightPin 8
 
 /*
  * mode --> 0 : manual mode, 1 : autonomous mode
- * data --> {acc, dec, direction}
+ * data --> {acc, dec, direction, light}
  *      acc : 0 ~ 5 V 
  *      dec : 0 ~ 5 V
  *      direction : 0 : forward, 1 : reverse
+ *      light : 0 : off, 1 : on
  * distance --> distance during moter running a cycle
  */
-int mode = 0;          
-volatile float data[3] = {0, 0, 0}; 
+int mode = 0;         
+volatile float data[4] = {0, 0, 0, 0}; 
 volatile float vel = 0; 
 float distance = diameter * pi / gear_ratio * 1000 * 3.6; 
 double previousTime = 0;
@@ -47,6 +50,7 @@ void setup() {
     pinMode(directPin, OUTPUT);  
     pinMode(modePin, INPUT);  
     pinMode(interruptPin, INPUT_PULLUP);
+    pinMode(lightPin, OUTPUT);
 }
 
 void loop() {
@@ -64,14 +68,16 @@ void getData() {
     int i = 0;
     char chr;
     if (Serial.available() > 0) {
-        data[0] = data[1] = data[2] = 0;
+        data[0] = data[1] = data[2] = data[3] = 0;
         
-        while ((chr = Serial.read()) != '\n' && i < 3) {
+        while ((chr = Serial.read()) != '\n' && i < 4) {
             if (chr == ',') i++;
             if (chr >= '0' && chr <= '9') data[i] = data[i] * 10 + (chr - '0');
         }
         if (data[2] == 1) digitalWrite(directPin, HIGH);
         else digitalWrite(directPin, LOW);
+        if (data[3] == 1) digitalWrite(lightPin, HIGH);
+        else digitalWrite(lightPin, LOW);
         data[0] = data[0] / 100.0 * 5;
         data[1] = data[1] / 100.0 * 5;
         
@@ -91,6 +97,8 @@ void showInfo() {
     Serial.print("Direction : ");
     if (data[2] == 1) Serial.println("Reverse");
     else Serial.println("Forward");
+    if (data[3] == 1) Serial.println("Lights ON");
+    else Serial.println("Lights OFF");
     return;
 }
 
