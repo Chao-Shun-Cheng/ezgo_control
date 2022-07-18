9cd124563b99bb5fed995736f7c904c077e029c5
ethanec <ethanec@gmail.com>
2022-07-18 14:44:06 +0800

    Revise document title

---
+++ src/arduino_code/ezgo_arduino/ezgo_arduino.ino
@@ -0,0 +1,109 @@
+/*
+ *  Testing Code for E-Z-GO Glof Car
+ *  This Package is able to control throttle, brake and light.
+ *  Besides, it has two control mode, manual mode, autonumous mode.
+ */
+#include "DAC8562.h"
+
+/*
+ * modePin    : This is mode change pin. (digital input)
+ * SSPin      : This pin controls DAC8562
+ * accPin     : This is accelerate pin.  (PWM output)
+ * directPin  : This is direction pin    (digital output)
+ * Clock      : Pin 13
+ * Din        : Pin 11
+ * diameter   : 18 inch tire
+ * gear_ratio : gear ratio is 16.99
+ * REF_POWER  : This power is for referencing by DAC8562
+ * lightPin   : This pin controls front and tail lights.
+ */
+#define modePin 7
+#define SSPin 10
+#define interruptPin 2
+#define directPin 4
+#define diameter 0.4572
+#define pi 3.14159
+#define gear_ratio 16.99
+#define REF_POWER 3.34
+#define lightPin 8
+
+/*
+ * mode --> 0 : manual mode, 1 : autonomous mode
+ * data --> {acc, dec, direction, light}
+ *      acc : 0 ~ 5 V
+ *      dec : 0 ~ 5 V
+ *      direction : 0 : forward, 1 : reverse
+ *      light : 0 : off, 1 : on
+ * distance --> distance during moter running a cycle
+ */
+int mode = 0;
+volatile float vel = 0;
+float distance = diameter * pi / gear_ratio * 1000 * 3.6;
+double previousTime = 0;
+DAC8562 dac = DAC8562(SSPin, REF_POWER);
+
+byte writer_buf[8] = {0};
+byte reader_buf[3] = {0};
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.setTimeout(200);
+    dac.begin();
+    attachInterrupt(digitalPinToInterrupt(interruptPin), calVel, RISING);
+    pinMode(directPin, OUTPUT);
+    pinMode(modePin, INPUT);
+    pinMode(interruptPin, INPUT_PULLUP);
+    pinMode(lightPin, OUTPUT);
+}
+
+void loop()
+{
+    int mode = digitalRead(modePin);
+    if (mode == HIGH) {
+        getData();
+    } 
+    showInfo();
+    delay(100);
+}
+
+void getData()
+{
+    Serial.readBytes(reader_buf, 3);
+    dac.writeA(((int) 0x00000000 | reader_buf[0]) / 255.0 * 5.0);
+    dac.writeB(((int) 0x00000000 | reader_buf[1]) / 255.0 * 5.0);
+    if (reader_buf[2] & 0x1) {
+        digitalWrite(directPin, HIGH);
+    } else {
+        digitalWrite(directPin, LOW);
+    }
+    return;
+}
+
+void showInfo()
+{
+    writer_buf[0] = reader_buf[0];
+    writer_buf[1] = reader_buf[1];
+    writer_buf[2] = reader_buf[2];
+    writer_buf[3] = ((byte) mode) & 0x1;
+    float current_vel = vel;
+    writer_buf[4] = ((byte) current_vel) & 0xff;
+    writer_buf[5] = (((byte) current_vel) >> 8) & 0xff;
+    writer_buf[6] = (((byte) current_vel) >> 16) & 0xff;
+    writer_buf[7] = (((byte) current_vel) >> 24) & 0xff;
+    Serial.write(writer_buf, 8);
+    return;
+}
+
+void calVel()
+{
+    static int count = 0;
+    count = count + 1;
+    if (count <= 3)
+        previousTime = millis();
+    else if (count >= 64) {
+        vel = distance / (millis() - previousTime);
+        count = 0;
+    }
+    return;
+}
